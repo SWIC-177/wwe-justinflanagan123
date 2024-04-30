4dc5670b46a029cb8534ed9b1e3d38dadd0f4d52
justinflangan123 <justin.flanagan@student.swic.edu>
2024-04-30 14:05:39 -0500

    feat: :wrench: Add filters
    Add filters to remove women's world champion and vacant title.

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 import { BELTS, CHAMPIONS } from "./src/data";
 
-console.log(BELTS, CHAMPIONS);
+const beltsWithoutWomen = BELTS.filter(
+  (belt) => belt !== "Women's World Champion",
+);
+
+const vacantTitle = CHAMPIONS.filter((belt) => belt !== "Vacant Title");
+
+console.log(beltsWithoutWomen, vacantTitle);
