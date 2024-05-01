969040ad6bac5a1cbc8a14297bef8771dc4e06d2
justinflangan123 <justin.flanagan@student.swic.edu>
2024-04-30 23:24:26 -0500

    feat: :art: Update sort function

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 import { CHAMPIONS } from "./src/data";
 
-const sortedChampions = CHAMPIONS.toSorted();
+import { getLastName } from "./src/lib";
+
+const sortedChampions = CHAMPIONS.toSorted((a, b) =>
+  getLastName(a).localeCompare(getLastName(b)),
+);
+
+/**
+ * 1. Sort the CHAMPIONS array by last name
+ * 2. SPLIT the fill name into first and last name
+ * 3. Compare just the last names
+ */
 
 console.log(sortedChampions);
