e3199ac6fea374373ce49bf35bf3d1edf5e27a2b
justinflangan123 <justin.flanagan@student.swic.edu>
2024-04-30 23:43:08 -0500

    Add array that contains title and champion

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { CHAMPIONS } from "./src/data";
+import { CHAMPIONS, BELTS } from "./src/data";
 
 import { getLastName } from "./src/lib";
 
@@ -6,10 +6,17 @@ const sortedChampions = CHAMPIONS.toSorted((a, b) =>
   getLastName(a).localeCompare(getLastName(b)),
 );
 
+const CHAMPIONSHIPS = BELTS.map((title, index) => ({
+  title,
+  champion: CHAMPIONS[index],
+}))
+  .sort((a, b) => a.champion.localeCompare(b.champion))
+  .filter((championship) => championship.champion !== "Vacant Title");
+
 /**
  * 1. Sort the CHAMPIONS array by last name
  * 2. SPLIT the fill name into first and last name
  * 3. Compare just the last names
  */
 
-console.log(sortedChampions);
+console.log(sortedChampions, CHAMPIONSHIPS);
